25b62a5429c87f7c71c62a29e86109316d85f79a
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-05-14 15:37:18 +0300

    setup redux

---
+++ src/app/components/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ConnectedTaskList from './TaskList';
+
+export const Dashboard = ({ groups }) => (
+  <div>
+    <h2>Dashboard</h2>
+    { groups.map(group => (
+      <ConnectedTaskList id={group.id} name={group.name} key={group.id}/>
+    ))}
+  </div>
+);
+
+const mapStateToProps = (state) =>  {
+  return { groups: state.groups };
+};
+
+export default connect(mapStateToProps)(Dashboard);
